ef5a1cabfc9cc4bc5526505d7b33a026509d275f
Muhammed Görkem Acır <86821706+gorkemacir@users.noreply.github.com>
2022-12-02 15:08:08 +0300

    Delete ecommerce-demo-pair3/ecommerce-demo-pair3 directory

--- ecommerce-demo-pair3/ecommerce-demo-pair3/src/main/java/com/etiya/ecommercedemopair3/repository/abstracts/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.etiya.ecommercedemopair3.repository.abstracts;
-
-import com.etiya.ecommercedemopair3.entities.concrets.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category,Integer> {
-}
--- ecommerce-demo-pair3/ecommerce-demo-pair3/src/main/java/com/etiya/ecommercedemopair3/repository/abstracts/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.etiya.ecommercedemopair3.repository.abstracts;
-
-import com.etiya.ecommercedemopair3.entities.concrets.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,Integer> {
-}
